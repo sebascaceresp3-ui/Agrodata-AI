7492aca9779555bf5a756058dcdb23c42bbab37e
sebascaceresp3-ui <sebascaceresp3@gmail.com>
2026-09-08 19:29:50 -0600

    Delete tests directory

--- tests/test_metrics.py
+++
@@ -1,5 +0,0 @@
-from utils.metrics import annual_change, cagr, safe_div
-def test_annual_change(): assert annual_change(110,100)==10
-def test_zero(): assert annual_change(1,0) is None
-def test_cagr(): assert round(cagr(100,121,2),6)==10
-def test_share(): assert safe_div(25,100)==.25
